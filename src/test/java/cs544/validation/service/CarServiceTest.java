828aba7444a7f093ce8bdfe00ebc21691dfb5f17
Yogendra Pant <pant.yogen@gmail.com>
2014-05-13 22:22:17 -0500

    Added test cases (dummy, commented)

---
+++ src/test/java/cs544/validation/service/CarServiceTest.java
@@ -0,0 +1,126 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cs544.validation.service;
+
+import cs544.validation.entity.Car;
+import java.util.List;
+import org.hibernate.SessionFactory;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Yogendra
+ */
+public class CarServiceTest {
+    
+    public CarServiceTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+//    /**
+//     * Test of setSessionFactory method, of class CarService.
+//     */
+//    @org.junit.Test
+//    public void testSetSessionFactory() {
+//        System.out.println("setSessionFactory");
+//        SessionFactory sf = null;
+//        CarService instance = new CarService();
+//        instance.setSessionFactory(sf);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of getAll method, of class CarService.
+//     */
+//    @org.junit.Test
+//    public void testGetAll() {
+//        System.out.println("getAll");
+//        CarService instance = new CarService();
+//        List<Car> expResult = null;
+//        List<Car> result = instance.getAll();
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of add method, of class CarService.
+//     */
+//    @org.junit.Test
+//    public void testAdd() {
+//        System.out.println("add");
+//        Car car = null;
+//        CarService instance = new CarService();
+//        instance.add(car);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of get method, of class CarService.
+//     */
+//    @org.junit.Test
+//    public void testGet() {
+//        System.out.println("get");
+//        int id = 0;
+//        CarService instance = new CarService();
+//        Car expResult = null;
+//        Car result = instance.get(id);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of update method, of class CarService.
+//     */
+//    @org.junit.Test
+//    public void testUpdate() {
+//        System.out.println("update");
+//        int carId = 0;
+//        Car car = null;
+//        CarService instance = new CarService();
+//        instance.update(carId, car);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of delete method, of class CarService.
+//     */
+//    @org.junit.Test
+//    public void testDelete() {
+//        System.out.println("delete");
+//        int carId = 0;
+//        CarService instance = new CarService();
+//        instance.delete(carId);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+    
+}
